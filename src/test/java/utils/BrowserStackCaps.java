f05490034d38b40663be58143bf50c8d93453c89
faizul <faizulcse@gmail.com>
2022-05-17 20:17:07 +0600

    Update BrowserStackCaps.java

--- src/test/java/utils/BrowserStackCaps.java
+++ src/test/java/utils/BrowserStackCaps.java
@@ -3,26 +3,28 @@ package utils;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserStackCaps {
-    public static final String PROJECT_NAME = "SELENIUM AUTOMATION PROJECT";
-    public static final String BUILD_NAME = System.getenv("BROWSERSTACK_BUILD_NAME");
-    public static final String GPS_LOCATION = "23.7746479,90.4031033";
-    public static final String TIMEZONE = "Dhaka";
-    public static String appId = BrowserStackApi.getRecentApp("test_app");
-    public static String testCaseName = "";
-    public static String deviceName = "";
-    public static String osVersion = "";
-    public static String platformName = "";
+    private static final String deviceProfile = System.getProperty("device") == null ? "bs_default" : System.getProperty("device");
+    private static final ResourceHelper bsDevice = new ResourceHelper().getResource(deviceProfile);
+
+    private static final String PROJECT_NAME = "SELENIUM AUTOMATION PROJECT";
+    private static final String BUILD_NAME = System.getenv("BROWSERSTACK_BUILD_NAME");
+    private static final String GPS_LOCATION = "23.7746479,90.4031033";
+    private static final String TIMEZONE = "Dhaka";
 
     public static DesiredCapabilities getBrowserStackCaps() throws Exception {
+        return getBrowserStackCaps("");
+    }
+
+    public static DesiredCapabilities getBrowserStackCaps(String testName) throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("device", deviceName);
-        caps.setCapability("os_version", osVersion);
-        caps.setCapability("platformName", platformName);
+        caps.setCapability("device", bsDevice.getString("device"));
+        caps.setCapability("os_version", bsDevice.getString("os_version"));
+        caps.setCapability("platformName", bsDevice.getString("platformName"));
         caps.setCapability("autoGrantPermissions", true);
         caps.setCapability("disableAnimations", true);
 
-        caps.setCapability("app", appId);
-        caps.setCapability("name", testCaseName);
+        caps.setCapability("app", BrowserStackApi.getRecentApp("test_app"));
+        caps.setCapability("name", testName);
         caps.setCapability("project", PROJECT_NAME);
         caps.setCapability("build", BUILD_NAME);
         caps.setCapability("browserstack.gpsLocation", GPS_LOCATION);
@@ -33,5 +35,6 @@ public class BrowserStackCaps {
 
     public static void main(String[] args) throws Exception {
         System.out.println(getBrowserStackCaps());
+        System.out.println(getBrowserStackCaps("faizulcse"));
     }
 }
